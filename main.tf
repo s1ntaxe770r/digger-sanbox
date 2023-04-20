60a1ab9ccfb9728f4e096c3e9de7cf0cc9939a47
s1ntaxe770r <cyberdev01@protonmail.com>
2023-04-21 00:09:13 +0100

    init main.tf with null resource

---
+++ main.tf
@@ -0,0 +1,9 @@
+// create a null resource with basic fields and a provisioner
+resource "null_resource" "null-tests" {
+    triggers = {
+        test = "test"
+    }
+    # provisioner "local-exec" {
+    #     command = "echo ${self.triggers.test}"
+    # }
+}
